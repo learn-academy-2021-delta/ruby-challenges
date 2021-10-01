1aa188e189f644cf99c43963aa900476fd5580d0
ciannadogan <dogan.cianna@yahoo.com>
2021-10-01 10:53:24 -0700

    completed two challenges of conditionals

---
+++ conditionals-cianna-rebecca.rb
@@ -0,0 +1,12 @@
+# Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+p 'Player one, please type rock, paper, or scissors.'
+user_one = gets.chomp.downcase
+
+# Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+p 'Player two please type rock, paper, or scissors.'
+user_two = gets.chomp.downcase
+
+# Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+
+# Story: As a user, I can see a message in the terminal saying if there was a tie.
+
