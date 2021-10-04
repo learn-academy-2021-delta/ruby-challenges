f4451bf73fdbfb82ddad2fbbff763a0343c2852d
brandonho2000 <bradho099@gmail.com>
2021-10-04 17:20:31 -0500

    worked on almost all

---
+++ methods-bh-my.rb
@@ -0,0 +1,6 @@
+# As a developer, I can create a method that checks for the following rules for a user ID and password:
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
