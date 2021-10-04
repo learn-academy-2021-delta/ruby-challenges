ad5afaf00a202a98b741221c1c24dc705431802e
LiangchengR <liangchengr@gmail.com>
2021-10-04 16:46:40 -0700

    completed hashes challenges

--- hashes-rebecca-kelly.rb
+++ hashes-rebecca-kelly.rb
@@ -20,16 +20,39 @@ my_phone[:maps] = my_phone.delete :facebook
 # p my_phone
 
 my_phone[:spotify] = my_phone.delete :instagram
-p my_phone
+# p my_phone
 # As a developer, I can update two values in my_phone.
 my_phone[:maps] = 'Navigation' 
 # p my_phone
 
 my_phone[:spotify] = 'Music'
-p my_phone
+# p my_phone
 
 # As a developer, I can delete two key:value pairs from my_phone.
+my_phone.delete(:reddit)
+my_phone.delete(:maps)
+# p my_phone
 
 # As a developer, I can use an enumerable method to return information about all of my_phone's applications.
+
+# my_phone.each do |key, value|
+#     p "#{key} is a #{value} app."
+# end
+
 # Stretch Challenges
 # As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
+
+# wants a method
+# input: hash
+# output: array
+
+#my_phone returns {:whatsapp=>"Messenger", :venmo=>"Money Transfer", :spotify=>"Music"}
+#:key => value
+
+def app_info hash
+    hash.map do |key, value|
+        "#{key.capitalize} is a #{value} app."
+    end
+end
+
+p app_info my_phone
