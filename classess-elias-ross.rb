0695aa7e9878d51fc6e13072eba0cca1de8c637b
EliasEritrea <eliaszerai@gmail.com>
2021-10-06 17:46:23 -0400

    partially completed

---
+++ classess-elias-ross.rb
@@ -0,0 +1,28 @@
+#As a developer, I can create a class called Task.
+
+class Task 
+end
+    # def set_title title1.title2,title3
+    #    @name=name
+    #    @age=age
+    #    @color=color
+    # end
+
+#As a developer, I can create three instances (objects) of class Task. 
+#(e.g laundry = Task.new)
+obj1 =Task.new
+obj2 =Task.new
+obj3 =Task.new
+#As a developer, I can initialize each instance of class Task with a title.
+
+
+# obj1.set_title 'apple',20,'orange'
+# obj2.set_title 'elias',10,'peach'
+# obj3.set_title 'Ross',20,'banna'
+# puts obj1
+# puts obj2
+# puts obj3
+
+# As a developer, I can see the title of each instance of class Task.
+# As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
+# As a developer, I can update the status of each instance of class Task when the task has been completed.
