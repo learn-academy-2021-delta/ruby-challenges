a84c44368b52ad55537c1cbe0fe57e1ca3cb7b89
JBrady <jbradysd@icloud.com>
2021-10-07 13:05:38 -0700

    finished a handful of challenges

---
+++ ruby-rspec/task-marisa-john_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require_relative 'task-marisa-john'
+
+describe 'Task' do
+    it 'has to be real' do
+        expect{ Task.new }.to_not raise_error
+    end
+    it 'has a title' do
+        my_task = Task.new
+        my_task.title = 'cleaning'
+        expect(my_task.title).to be_a String
+    end
+    it 'has a description' do
+        my_task = Task.new
+        my_task.description = 'cleaning the house'
+        expect(my_task.description).to eq 'cleaning the house'
+        expect(my_task.description).to be_a String
+    end
+    it 'has a progress status' do
+        my_task = Task.new
+        my_task.status = 'in progress'
+        expect(my_task.status).to eq 'in progress'
+        expect(my_task.status).to be_a String
+    end
+    it 'can be marked as completed' do
+        my_task = Task.new
+        my_task.complete = 'completed'
+        expect(my_task.complete).to eq 'completed'
+        # expect{my_task.complete 'completed'}.to change{my_task.status}.from('in progress').to('completed')
+        expect(my_task.complete).to start_with('in progress').and end_with('completed')
+    end
+end
